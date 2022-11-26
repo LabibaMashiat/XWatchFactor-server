a03e3bcecc08a54fb85822fbddc6745eb86cee2b
LabibaMashiat20 <labibamashiatmahi@gmail.com>
2022-11-26 19:23:52 +0600

    env setup

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const port=process.env.PORT || 5000;
+require('dotenv').config();
 const app=express();
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
+//mongodb starting
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ksontsm.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+//mongodb ending
 app.get('/',async(req,res)=>{
     res.send('x-watch-factor server is running');
 });
